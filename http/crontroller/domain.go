d66c9db6a5ea46591589a765862997fc17139168
95028555@qq.com <yangyi199159>
2019-08-16 14:13:58 +0800

    finish

--- http/crontroller/domain.go
+++ http/crontroller/domain.go
@@ -2,23 +2,26 @@ package crontroller
 
 import (
 	"comadmin/model/admin"
+	"comadmin/pkg/app"
 	"comadmin/tools/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func (h HttpHandler) CreateDomain(c *gin.Context) {
+func (h HttpHandler) CreateDomain(c app.GContext) {
+	g := app.G{c}
 	name := c.Query("name")
 	domain := admin.Domain{Name: name, Id: utils.EncodeMd5(name)}
 	code := h.logic.Create(domain)
-	c.JSON(http.StatusOK, code)
+	g.Json(http.StatusOK, code, "")
 	return
 }
 
 func (h HttpHandler) DeleteDoDomain(c *gin.Context) {
+	g := app.G{c}
 	id := c.Query("id")
 	domain := admin.Domain{Id: id}
 	code := h.logic.Delete(domain)
-	c.JSON(http.StatusOK, code)
+	g.Json(http.StatusOK, code, "")
 	return
 }
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,8 +1,8 @@
 package app
 
 import (
+	"comadmin/pkg/e"
 	"github.com/gin-gonic/gin"
-	"wxadmin/pkg/e"
 )
 
 type GContext = *gin.Context
@@ -11,12 +11,10 @@ type G struct {
 	*gin.Context
 }
 
-func (g *G) Json(httpCode, code int, action, data interface{}) {
+func (g *G) Json(httpCode, code int, data interface{}) {
 	m := make(map[string]interface{})
 	m["code"] = code
 	m["msg"] = e.GetMsg(code)
-	m["action"] = action
 	m["data"] = data
 	g.JSON(httpCode, m)
-	return
 }
