d860639e58d9fc100f1f3c14c72074230f0e2ec3
xiaohan <yangyi199159>
2019-08-22 21:55:58 +0800

    wx接口

--- pkg/config/load.go
+++ pkg/config/load.go
@@ -6,16 +6,16 @@ import (
 	"io/ioutil"
 )
 
-func Load(filename string, v interface{}) {
-	data, err := ioutil.ReadFile(filename)
+func Load(path string, v interface{}) {
+	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		fmt.Println("load读取配置文件出错--->", filename)
+		fmt.Println("load读取配置文件出错--->", path)
 		panic(err)
 	}
 
 	err = json.Unmarshal(data, v)
 	if err != nil {
-		fmt.Println("load序列化配置文件出错--->", filename)
+		fmt.Println("load序列化配置文件出错--->", path)
 		panic(err)
 	}
 }
