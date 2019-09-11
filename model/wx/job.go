9ff63f9cdef53a61baabbcdb569d6e61b9f6a089
95028555@qq.com <yangyi199159>
2019-09-11 14:33:42 +0800

    队列任务

--- model/wx/job.go
+++ model/wx/job.go
@@ -7,10 +7,11 @@ import "time"
 */
 
 type Job struct {
-	Id     string    `json:"id"`                    //客户端id
-	Ip     string    `json:"ip"`                    //客户端ip
-	Ctime  time.Time `json:"ctime"  xorm:"created"` //注册时间
-	Count  int       `json:"count"`                 //运行次数
-	Status int       `json:"status"`                //是否在线运行
-	Etime  time.Time `json:"etime" xorm:"updated"`  //运行结束时间
+	Id       string    `json:"id"`                    //客户端id
+	Ip       string    `json:"ip"`                    //客户端ip
+	Ctime    time.Time `json:"ctime"  xorm:"created"` //注册时间
+	Count    int       `json:"count"`                 //运行次数
+	JobCount int       `json:"job_count"`             //任务数量
+	Status   int       `json:"status"`                //是否在线运行
+	Etime    time.Time `json:"etime" xorm:"updated"`  //运行结束时间
 }
